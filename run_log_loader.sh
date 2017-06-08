ee6ee3182b1764c4874728a561c7c2816d01b7c8
ru.dev26 <rudev26@ru.dev26>
2017-06-08 11:51:37 +0300

    +

--- run_log_loader.sh
+++ run_log_loader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir logs
-python3 logdb_server.py 95.163.100.29 9011 --loglevel=WARNING --test-queue 2>&1 | tee -a logs/android_log_loader.log &
+python3 logdb_server.py 95.163.100.29 9011 --loglevel=INFO --test-queue 2>&1 | tee -a logs/android_log_loader.log &
 
 exit 0;
